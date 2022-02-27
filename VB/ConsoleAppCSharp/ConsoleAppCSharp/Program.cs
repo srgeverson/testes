0ae7120c830f9375342e63ac562ca6f78275e376
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-02-26 22:16:21 -0300

    Curso de VB básico

---
+++ VB/ConsoleAppCSharp/ConsoleAppCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
