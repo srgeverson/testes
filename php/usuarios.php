da9cc6e7fc87c868661922b08f146ad724c6b1c7
Geverson <geversonjosedesouza@gmail.com>
2022-05-25 00:41:43 -0300

    Ajustste na autenticação do usuário

--- php/usuarios.php
+++ php/usuarios.php
@@ -12,7 +12,7 @@ class User {
     public function conectar($nomeDB, $host, $user, $password) {
         if (!isset($this->instance)) {
             try {
-                $this->instance = new PDO("mysql:host=" . $host . ";dbname=" . $nomeDB . ";", $user, $pass, $this->comp);
+                $this->instance = new PDO("mysql:host=" . $host . ";dbname=" . $nomeDB . ";port=3306;", $user, $password, $this->comp);
                 $this->instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
             } catch (Exception $erro) {
@@ -20,6 +20,20 @@ class User {
             }
         }
         return $this->instance;
+    }
+
+    public function logar($user, $password) {
+        try {
+            $query = "SELECT * FROM user WHERE name=:name; AND password = :password";
+            $conexao = $this->instance;
+            $statement = $conexao->prepare($query);
+            $statement->bindParam(":name", $name, PDO::PARAM_STR);
+            $statement->bindParam(":password", $password, PDO::PARAM_STR);
+            $statement->execute();
+        } catch (Exception $erro) {
+            throw new Exception($erro->getMessage());
+        }
+        return true;
     }
 
 }
--- php/validar.php
+++ php/validar.php
@@ -7,7 +7,7 @@
         $passwd = addslashes($_POST['senha']);
 
         if (!empty($user) && !empty($passwd)){
-            $u->conectar("mystore", "localhost", $user, $passwd);
+            $u->conectar("teste", "127.0.0.1", $user, $passwd);
             if ($u->msgErro == ""){
                 if (!$u->logar($user, $passwd)){
                     echo "Credenciais incorretas!";
