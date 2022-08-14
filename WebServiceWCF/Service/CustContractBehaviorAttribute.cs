f4aff97200ae4f1e8b4efd69e8e805e77ddb2408
Geverson <geversonjosedesouza@gmail.com>
2022-08-14 13:30:44 -0300

    Interceptando as requisições

---
+++ WebServiceWCF/Service/CustContractBehaviorAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace WebServiceWCF
+{
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = false)]
+    public class CustContractBehaviorAttribute : Attribute, IContractBehavior, IContractBehaviorAttribute
+    {
+        public Type TargetContract => throw new NotImplementedException();
+
+        public void AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+            return;
+        }
+
+        public void ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            clientRuntime.ClientMessageInspectors.Add(new ClientMessageLogger());
+        }
+
+        public void ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, DispatchRuntime dispatchRuntime)
+        {
+            dispatchRuntime.MessageInspectors.Add(new ServerMessageLogger());
+        }
+
+        public void Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
+        {
+            return;
+        }
+    }
+}
---
+++ WebServiceWCF/Service/ServerMessageLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Web;
+
+namespace WebServiceWCF
+{
+    public class ServerMessageLogger : IDispatchMessageInspector
+    {
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            //string token = null;
+            //IncomingWebRequestContext request = WebOperationContext.Current.IncomingRequest;
+            //token = request.Headers["Authorization"].ToString().Replace("Bearer", "").Trim();
+            return null;
+        }
+
+     
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            WebOperationContext ctx = WebOperationContext.Current;
+            ctx.OutgoingResponse.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
