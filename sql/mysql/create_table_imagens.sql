914bf2631fb0628487eaed15804ab78acec66777
Geverson <geversonjosedesouza@gmail.com>
2022-08-24 22:21:15 -0300

    Movido para o projeto https://github.com/srgeverson/sql

--- sql/mysql/create_table_imagens.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `imagens` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(220) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `imagem` varchar(220) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
